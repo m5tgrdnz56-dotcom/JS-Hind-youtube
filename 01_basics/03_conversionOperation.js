7b8aa1a926929949f0c89886a12f2d2539df09e9
m5tgrdnz56-dotcom <m5tgrdnz56@privaterelay.appleid.com>
2026-05-06 11:13:07 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score = "33abc"
-console.log(typeof(score));
-console.log(typeof score);
- let valueInNumber = Number(score)
- console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let score = "33abc"
+// console.log(typeof(score));
+// console.log(typeof score);
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "22abc" => NaN
@@ -12,7 +12,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Chandu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
  
 // 1 => true; 0 => false
 // "" => false
@@ -20,6 +20,23 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ********************Operations**************
+
+// let value = 34
+// let negValue = -value
+// console.log(negValue);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 1 + 2); //112
+// console.log(2 + 1 + "3"); // 33
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
